b52a62aeaa3e18270014822a65e2fbca4bb53ef1
ptrckzuca <116358763+ptrckzuca@users.noreply.github.com>
2024-04-27 04:47:06 +0100

    Add files via upload

---
+++ requisito_5.h
@@ -0,0 +1,6 @@
+#include "monteCarlo.h"
+
+#ifndef MONTECARLO_REQUISITO_05_H
+#define MONTECARLO_REQUISITO_05_H
+void requisito_5(char *filename, int num_processes, int total_points_generated, Point polygon[], int n);
+#endif //MONTECARLO_REQUISITO_05_H
