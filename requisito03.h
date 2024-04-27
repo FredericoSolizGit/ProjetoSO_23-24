d313be8c0fb8202a164c1c9df83649cfe41daea6
FredericoSolizGit <146753561+FredericoSolizGit@users.noreply.github.com>
2024-04-27 16:05:56 +0100

    Delete requisito03.h

--- requisito03.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by so on 20-04-2024.
-//
-#include "monteCarlo.h"
-#ifndef MONTECARLO_REQUISITO03_H
-#define MONTECARLO_REQUISITO03_H
-void child_process(int id, int pipes[2], Point polygon[], int n, const char* filename);
-#endif //MONTECARLO_REQUISITO03_H
